9de92b6271ec8a76efeb37a38a82d25c8ed08796
rommick <siame.romain.scw@gmail.com>
2026-02-26 17:54:05 +0100

    feat(Compétences): classer les compétences par catégorie

--- frontend/src/pages/CompetencesFr.vue
+++ frontend/src/pages/CompetencesFr.vue
@@ -2,14 +2,26 @@
   <div class="competences-page">
     <section class="neo-section neo-min py-5">
       <div class="container neo-content">
-        <h2 class="text-center mb-5 scroll-animate neo-heading">Langages & Technologies</h2>
-        <div class="row scroll-stagger justify-content-center">
-          <div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-4" v-for="tech in technologies" :key="tech.id">
-            <div class="neo-tech-card text-center p-3">
-              <div class="neo-tech-logo-container mb-3 mx-auto">
-                <img :src="tech.logo" :alt="tech.name" class="neo-tech-logo" />
+        <h2 class="text-center mb-5 scroll-animate neo-heading">Langages &amp; Technologies</h2>
+        <div class="scroll-stagger">
+          <div class="mb-5" v-for="category in technologyCategories" :key="category.id">
+            <div class="d-flex align-items-center justify-content-between mb-3">
+              <h4 class="mb-0 neo-subheading">{{ category.title }}</h4>
+              <small v-if="category.note" class="text-muted">{{ category.note }}</small>
+            </div>
+            <div class="row justify-content-center">
+              <div
+                class="col-6 col-sm-4 col-md-3 col-lg-2 mb-4"
+                v-for="tech in category.items"
+                :key="tech.name"
+              >
+                <div class="neo-tech-card text-center p-3">
+                  <div class="neo-tech-logo-container mb-3 mx-auto">
+                    <img :src="tech.logo" :alt="tech.name" class="neo-tech-logo" />
+                  </div>
+                  <h6 class="neo-tech-name">{{ tech.name }}</h6>
+                </div>
               </div>
-              <h6 class="neo-tech-name">{{ tech.name }}</h6>
             </div>
           </div>
         </div>
@@ -71,81 +83,56 @@ export default {
   },
   data() {
     return {
-      technologies: [
+      technologyCategories: [
         {
-          id: 1,
-          name: 'Vue.js',
-          logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg'
+          id: 'front',
+          title: 'Front-end & Mobile',
+          note: 'Interfaces, UX et apps hybrides',
+          items: [
+            { name: 'Vue.js', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg' },
+            { name: 'Flutter', logo: '/img/tech-logos/Flutter.png' },
+            { name: 'Dart', logo: '/img/tech-logos/Dart.png' }
+          ]
         },
         {
-          id: 2,
-          name: 'Node.js',
-          logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
+          id: 'back',
+          title: 'Back-end & APIs',
+          note: 'Services, logique métier et architectures web',
+          items: [
+            { name: 'Node.js', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg' },
+            { name: 'Python', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' },
+            { name: 'PHP', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg' },
+            { name: 'Symfony', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/symfony/symfony-original.svg' },
+            { name: 'REST API', logo: '/img/tech-logos/APIRest.png' }
+          ]
         },
         {
-          id: 3,
-          name: 'Python',
-          logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg'
+          id: 'data',
+          title: 'Données & Bases',
+          note: 'Stockage relationnel et NoSQL',
+          items: [
+            { name: 'SQL', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg' },
+            { name: 'NoSQL', logo: '/img/tech-logos/Nosql.png' }
+          ]
         },
         {
-          id: 4,
-          name: 'PHP',
-          logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg'
+          id: 'systems',
+          title: 'Langages systèmes',
+          note: 'Performance, bas niveau et algorithmique',
+          items: [
+            { name: 'Java', logo: '/img/tech-logos/Java.png' },
+            { name: 'C', logo: '/img/tech-logos/C.png' },
+            { name: 'C++', logo: '/img/tech-logos/C++.png' }
+          ]
         },
         {
-          id: 5,
-          name: 'Symfony',
-          logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/symfony/symfony-original.svg'
-        },
-        {
-          id: 6,
-          name: 'Kubernetes',
-          logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg'
-        },
-        {
-          id: 7,
-          name: 'Docker',
-          logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg'
-        },
-        {
-          id: 8,
-          name: 'SQL',
-          logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
-        },
-        {
-          id: 9,
-          name: 'NoSQL',
-          logo: '/img/tech-logos/Nosql.png'
-        },
-        {
-          id: 10,
-          name: 'Java',
-          logo: '/img/tech-logos/Java.png'
-        },
-        {
-          id: 11,
-          name: 'C',
-          logo: '/img/tech-logos/C.png'
-        },
-        {
-          id: 12,
-          name: 'C++',
-          logo: '/img/tech-logos/C++.png'
-        },
-        {
-          id: 13,
-          name: 'Flutter',
-          logo: '/img/tech-logos/Flutter.png'
-        },
-        {
-          id: 14,
-          name: 'Dart',
-          logo: '/img/tech-logos/Dart.png'
-        },
-        {
-          id: 15,
-          name: 'REST API',
-          logo: '/img/tech-logos/APIRest.png'
+          id: 'devops',
+          title: 'DevOps & Delivery',
+          note: 'Conteneurs, orchestration et CI/CD',
+          items: [
+            { name: 'Docker', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg' },
+            { name: 'Kubernetes', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg' }
+          ]
         }
       ],
       competences: [
